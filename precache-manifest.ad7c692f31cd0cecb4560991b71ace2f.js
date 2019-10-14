3fc0945940b2999b09358bcc04b84e233f435d41
sawuer <ruslan.timurziyev@gmail.com>
2019-10-15 00:05:26 +0600

    Updates

---
+++ precache-manifest.ad7c692f31cd0cecb4560991b71ace2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "701d7afab1b2121b087b7fab24f0a03d",
+    "url": "/react-w/index.html"
+  },
+  {
+    "revision": "24124981e1951374470d",
+    "url": "/react-w/static/css/main.c1fc35c7.chunk.css"
+  },
+  {
+    "revision": "4c991e6c28e7c75a4dc2",
+    "url": "/react-w/static/js/2.8e21131d.chunk.js"
+  },
+  {
+    "revision": "24124981e1951374470d",
+    "url": "/react-w/static/js/main.2fe9bdd6.chunk.js"
+  },
+  {
+    "revision": "5a86cf0ad4a0b83744a2",
+    "url": "/react-w/static/js/runtime-main.83423ad5.js"
+  }
+]);
