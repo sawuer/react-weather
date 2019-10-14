cb8e5be8a282ac30ebeca120a496c0cd8ee64488
sawuer <ruslan.timurziyev@gmail.com>
2019-10-15 03:14:25 +0600

    Updates

--- precache-manifest.d16cda9562285ee002ae2c9ebb463138.js
+++ precache-manifest.d16cda9562285ee002ae2c9ebb463138.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "701d7afab1b2121b087b7fab24f0a03d",
+    "revision": "494fb407a29f9fc004e0576c8893e949",
     "url": "/react-w/index.html"
   },
   {
-    "revision": "24124981e1951374470d",
-    "url": "/react-w/static/css/main.c1fc35c7.chunk.css"
+    "revision": "86b4146edb98389fff90",
+    "url": "/react-w/static/css/main.4cfe9de2.chunk.css"
   },
   {
     "revision": "4c991e6c28e7c75a4dc2",
     "url": "/react-w/static/js/2.8e21131d.chunk.js"
   },
   {
-    "revision": "24124981e1951374470d",
-    "url": "/react-w/static/js/main.2fe9bdd6.chunk.js"
+    "revision": "86b4146edb98389fff90",
+    "url": "/react-w/static/js/main.e465c784.chunk.js"
   },
   {
     "revision": "5a86cf0ad4a0b83744a2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-w/precache-manifest.ad7c692f31cd0cecb4560991b71ace2f.js"
+  "/react-w/precache-manifest.d16cda9562285ee002ae2c9ebb463138.js"
 );
 
 self.addEventListener('message', (event) => {
